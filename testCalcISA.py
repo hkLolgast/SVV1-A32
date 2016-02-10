f41221b90d4e2710f8f10e9aeb357aba8bac1155
Rick <rick_molenaar96@live.nl>
2016-02-10 08:57:48 +0100

    Edited test file to be correct

--- testCalcISA.py
+++ testCalcISA.py
@@ -7,18 +7,19 @@ import unittest
 from CalcISA import *
 
 class TestCalcISA(unittest.TestCase):
-    def testmeter(self):
-        self.fail("Not implemented")
-
     def testcalcHeight(self):
         self.fail("Not implemented")
 
     def testvaluesAtHeight(self):
         self.fail("Not implemented")
 
+    def testmeter(self):
+        self.assertAlmostEqual(0.3048, meter(1))
+        self.assertAlmostEqual(15*0.3048,meter(15))
+        
     def testft(self):
-        self.assertEquals(1/0.3047, ft(1))
-        self.assertEqual(15/0.3047, ft(15))
+        self.assertAlmostEqual(1/0.3048, ft(1))
+        self.assertAlmostEqual(15/0.3048, ft(15))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
