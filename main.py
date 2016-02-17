Fixed boom area calculation not using centroid location

main.py
@@ -96,27 +96,11 @@ def realMomentOfInertia(axis, R, ts, floorHeight, tf, hst, wst, tst):
         floorAngle = np.arccos((R-floorHeight)/R)
         floorLength = 2*R*np.sin(floorAngle)
         
-        sumAd = 0
-        sumA = 0
-        
-        #fuselage
-        sumAd+=np.pi*2*R*ts*0
-        sumA +=np.pi*2*R*ts
-        
-        #floor
-        sumAd+=floorLength*tf*(-(R-floorHeight))
-        sumA +=floorLength*tf
-        
-        #stiffeners
-        stiffenerArea = hst*tst+wst*tst
-        for (x,y) in boomLocations(36, R, False, floorHeight):
-            sumAd+=stiffenerArea*y
-            sumA +=stiffenerArea
-            
-        Cy = sumAd/sumA
+        Cx, Cy = realCentroid(R, ts, floorHeight, tf, hst, wst, tst)
         
         I+=np.pi*R**3*ts+np.pi*2*R*ts*(0-Cy)**2
         I+=1/12.*floorLength*tf**3+floorLength*tf*(-(R-floorHeight)-Cy)**2
+        stiffenerArea = hst*tst+wst*tst
         
         for (x,y) in boomLocations(36, R, False, floorHeight):
             I+=stiffenerArea*(y-Cy)**2
@@ -134,6 +118,31 @@ def realMomentOfInertia(axis, R, ts, floorHeight, tf, hst, wst, tst):
         raise ValueError, "axis must be either x or y or xy"
 
     return I
+
+def realCentroid(R, ts, floorHeight, tf, hst, wst, tst):
+    floorAngle = np.arccos((R-floorHeight)/R)
+    floorLength = 2*R*np.sin(floorAngle)
+    
+    sumAd = 0
+    sumA = 0
+    
+    #fuselage
+    sumAd+=np.pi*2*R*ts*0
+    sumA +=np.pi*2*R*ts
+    
+    #floor
+    sumAd+=floorLength*tf*(-(R-floorHeight))
+    sumA +=floorLength*tf
+    
+    #stiffeners
+    stiffenerArea = hst*tst+wst*tst
+    for (x,y) in boomLocations(36, R, False, floorHeight):
+        sumAd+=stiffenerArea*y
+        sumA +=stiffenerArea
+        
+    Cy = sumAd/sumA
+    
+    return (0,Cy)               #Symmetry -> Cx = 0
 
 def shearCenter(booms):
     Cx = 0
@@ -172,4 +181,5 @@ if __name__=="__main__":
     hst = 0.015
     wst = 0.02
     tst = 0.012
-    print structuralAnalysis.boomAreas(Mx, My, booms, R, ts, fh, tf, hst, wst, tst)
+    for A in structuralAnalysis.boomAreas(Mx, My, booms, R, ts, fh, tf, hst, wst, tst):
+        print A

structuralAnalysis.py
@@ -9,12 +9,12 @@ B_n = sum([t_D*b/6*(2+sig_m/sig_n) for n in adjacent_booms]
 s1/s2 = (My/Iyy*x1+Mx/Ixx*y1)/(My/Iyy*x2+Mx/Ixx*Iyy)
 '''
 
-from main import realMomentOfInertia
+import main
 import numpy as np
 
 def boomAreas(Mx, My, booms, R, ts, floorHeight, tf, hst, wst, tst):
-    Ixx = realMomentOfInertia("x", R, ts, floorHeight, tf, hst, wst, tst)
-    Iyy = realMomentOfInertia("y", R, ts, floorHeight, tf, hst, wst, tst)
+    Ixx = main.realMomentOfInertia("x", R, ts, floorHeight, tf, hst, wst, tst)
+    Iyy = main.realMomentOfInertia("y", R, ts, floorHeight, tf, hst, wst, tst)
     stressBases = [0]*len(booms)
     floorDist1 = [0]*len(booms)
     floorDist2 = [0]*len(booms)
@@ -23,8 +23,11 @@ def boomAreas(Mx, My, booms, R, ts, floorHeight, tf, hst, wst, tst):
     floorLength = R*np.sin(floorAngle)
     floorAttach1 = (floorLength, floorHeight - R)
     floorAttach2 = (-floorLength, floorHeight - R)
+    
+    Cx, Cy = main.realCentroid(R, ts, floorHeight, tf, hst, wst, tst)
+    
     for i,(x,y) in enumerate(booms):
-        stressBases[i] = My/Iyy*x+Mx/Ixx*y
+        stressBases[i] = My/Iyy*(x-Cx)+Mx/Ixx*(y-Cy)
         floorDist1[i] = ((x-floorAttach1[0])**2+(y-floorAttach1[1])**2)**0.5
         floorDist2[i] = ((x-floorAttach2[0])**2+(y-floorAttach2[1])**2)**0.5
     