3fd798cc6148be9fb87d32daabe096d4d78643af
Gerjan <g.nieuwerth@hotmail.com>
2016-02-18 15:16:23 +0100

    removed useless comments in momentcalculationx

--- momentcalculationx.py
+++ momentcalculationx.py
@@ -4,8 +4,7 @@ Created on Wed Feb 17 11:50:32 2016
 
 @author: Gerjan
 """
-#nsections = [i for i in range(1,100)]
-#j = 1
+
 
 import numpy as np
 from numpy.linalg import solve
@@ -16,18 +15,17 @@ Sxlgm = 391680
 Mx = [0]*300
 
 for i in range(1,300):
- #   z = i/10
+
     moments = 0
     if i<135:
         Mx[i] = Sx*(i/10+2.8)
-#        moments[i].append(Mx)
+
     if i>=135 and i<260:
         Mx[i] = Sx*(i/10+2.8) - Sxlgm*(i/10-13.5)
- #       moments[i].append(Mx)
+
     if i>=260:
         Mx[i] = 0
-  #      moments[i].append(Mx)
-  #  print(Mx)
+
         
 plt.plot(Mx)
    
